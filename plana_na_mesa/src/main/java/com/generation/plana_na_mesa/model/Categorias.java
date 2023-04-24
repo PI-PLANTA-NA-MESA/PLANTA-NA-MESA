1b72fbb3c4e9e2c8b1d4dbb4f11034408049be1a
AzvalSG <giovanni0205.guimaraes@gmail.com>
2023-04-24 14:25:26 -0300

    Correção

--- plana_na_mesa/src/main/java/com/generation/plana_na_mesa/model/Categorias.java
+++ plana_na_mesa/src/main/java/com/generation/plana_na_mesa/model/Categorias.java
@@ -16,43 +16,44 @@ import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-	@Entity
-	@Table(name = "tb_categorias")
-	public class Categorias {
-		@Id
-		@GeneratedValue(strategy = GenerationType.IDENTITY)
-		private Long id;
-		
-		@NotBlank(message = "Esse é um campo obrigatório!")
-		@Size(min = 5, max = 255, message = "Limite de caracteres 255")
-		private String descricao;
-		
-		@NotBlank(message = "Esse é um campo obrigatório!")
-		@Size(min = 5, max = 255, message = "Limite de caracteres 255")
-		private String nome;
 
-		public Long getId() {
-			return id;
-		}
+@Entity
+@Table(name = "tb_categorias")
+public class Categorias {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-		public void setId(Long id) {
-			this.id = id;
-		}
+	@NotBlank(message = "Esse é um campo obrigatório!")
+	@Size(min = 5, max = 255, message = "Limite de caracteres 255")
+	private String descricao;
 
-		public String getDescricao() {
-			return descricao;
-		}
+	@NotBlank(message = "Esse é um campo obrigatório!")
+	@Size(min = 5, max = 255, message = "Limite de caracteres 255")
+	private String nome;
 
-		public void setDescricao(String descricao) {
-			this.descricao = descricao;
-		}
-
-		public String getNome() {
-			return nome;
-		}
-
-		public void setNome(String nome) {
-			this.nome = nome;
-		}
-
+	public Long getId() {
+		return id;
 	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+}
